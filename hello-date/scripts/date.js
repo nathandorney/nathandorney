d08aa80b6cb12a1445e0afb96ff1e6136ac87d32
Nathan Dorney <hello@nathandorney.co.uk>
2021-02-19 15:11:04 +0000

    Delete hello-date directory

--- hello-date/scripts/date.js
+++
@@ -1,6 +0,0 @@
-var d = new Date();
-
-var days = ["Sunday", "Monday", "Tuesday", "Wednesday",
-"Thursday", "Friday", "Saturday"];
-
-document.getElementById("display-day").innerHTML = days[d.getDay()];
